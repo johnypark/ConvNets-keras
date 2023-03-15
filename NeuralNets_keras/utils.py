d00c99806665aa462756759ede11306946ed3327
johnypark <parkjohnyc@gmail.com>
2023-03-15 09:43:36 -0400

    Add DropConnect

--- NeuralNets_keras/utils.py
+++ NeuralNets_keras/utils.py
@@ -3,41 +3,39 @@
 import tensorflow as tf
 from tensorflow import keras 
 
-# modified from tensorflow addons https://github.com/tensorflow/addons/blob/v0.17.0/tensorflow_addons/layers/stochastic_depth.py#L5-L90
-class StochasticDepth(tf.keras.layers.Layer):
+from keras import backend
+from keras.engine.base_layer import Layer
+from keras.utils import tf_utils
 
-    def __init__(self, survival_probability: float = 0.9, **kwargs):
-        super().__init__(**kwargs)
+## from https://github.com/keras-team/keras/blob/e6784e4302c7b8cd116b74a784f4b78d60e83c26/keras/layers/merging/base_merge.py#L1
 
-        self.survival_probability = survival_probability
+class DropConnect(keras.layers.Layer):
+    def __init__(self, rate, data_type =tf.float32, **kwargs):
+        super(DropConnect, self).__init__(**kwargs)
+        self.rate = rate
+        self.data_type = data_type
 
-    def call(self, x, training=None):
-        if not isinstance(x, list) or len(x) != 2:
-            raise ValueError("input must be a list of length 2.")
+    def build(self, input_shape):
+        shape = input_shape[-1] 
+        # Sample Bernoulli Random Variable
+        boolean = tf.random.uniform(shape =(1,), minval=0, maxval = 1) > self.rate
+        # Make mask_matrix
+        mask_matrix = tf.repeat(boolean, shape*shape)
+        mask_matrix = tf.reshape(mask_matrix, (shape, shape))
+        mask_matrix = tf.cast(mask_matrix, dtype = self.data_type)
+        # Apply mask to the input
+        self.StochasticDrop = keras.layers.Lambda(lambda x: tf.matmul(x, mask_matrix))
 
-        shortcut, residual = x
-
-        # Random bernoulli variable indicating whether the branch should be kept or not or not
-        b_out = keras.backend.random_bernoulli(
-            [], p=self.survival_probability
-        )
-
-        def _call_train():
-            return tf.keras.layers.Add()(shortcut, b_out * residual)
-
-        def _call_test():
-            return tf.keras.layers.Add()(shortcut, residual)
-
-        return tf.keras.backend.in_train_phase(
-            _call_train, _call_test, training=training
-        )
-
+    def call(self, inputs, training = None):
+        if training:
+            outputs = self.StochasticDrop(inputs)
+            return outputs
+        return inputs
+    
     def compute_output_shape(self, input_shape):
         return input_shape[0]
 
     def get_config(self):
         base_config = super().get_config()
-
-        config = {"survival_probability": self.survival_probability}
-
+        config = {"drop_rate": self.rate}
         return {**base_config, **config}

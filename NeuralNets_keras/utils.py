ff2b0b57b170b45d3ad72706650852c17b588404
johnypark <parkjohnyc@gmail.com>
2023-03-15 11:21:50 -0400

    Test

--- NeuralNets_keras/utils.py
+++ NeuralNets_keras/utils.py
@@ -9,9 +9,9 @@ from keras.utils import tf_utils
 
 ## from https://github.com/keras-team/keras/blob/e6784e4302c7b8cd116b74a784f4b78d60e83c26/keras/layers/merging/base_merge.py#L1
 
-class DropConnect(keras.layers.Layer):
+class DropConnect_builtin(keras.layers.Layer):
     def __init__(self, rate, **kwargs):
-        super(DropConnect, self).__init__(**kwargs)
+        super(DropConnect_builtin, self).__init__(**kwargs)
         self.rate = rate
 
     def build(self, input_shape):
@@ -21,6 +21,38 @@ class DropConnect(keras.layers.Layer):
         
     def call(self, inputs, training = None):
         return self.StochasticDrop(inputs)
+    
+    def compute_output_shape(self, input_shape):
+        return input_shape[0]
+
+    def get_config(self):
+        base_config = super().get_config()
+        config = {"drop_rate": self.rate}
+        return {**base_config, **config}
+    
+    
+    
+class DropConnect(keras.layers.Layer): # this does not behave well, why?
+    def __init__(self, rate, **kwargs):
+        super(DropConnect, self).__init__(**kwargs)
+        self.rate = rate
+
+    def build(self, input_shape):
+        shape = input_shape[-1] 
+        # Sample Bernoulli Random Variable
+        boolean = tf.random.uniform(shape =(1,), minval=0, maxval = 1) > self.rate
+        # Make mask_matrix
+        mask_matrix = tf.repeat(boolean, shape*shape)
+        mask_matrix = tf.reshape(mask_matrix, (shape, shape))
+        mask_matrix = tf.cast(mask_matrix, dtype = self.compute_dtype)
+        # Apply mask to the input
+        self.StochasticDrop = keras.layers.Lambda(lambda x: tf.matmul(x, mask_matrix))
+
+    def call(self, inputs, training = None):
+        if training:
+            outputs = self.StochasticDrop(inputs)
+            return outputs
+        return self.rate*inputs
     
     def compute_output_shape(self, input_shape):
         return input_shape[0]

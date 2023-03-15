2f9a1208b528ba2c8284be68730b4af22d47b877
johnypark <parkjohnyc@gmail.com>
2023-03-15 11:07:40 -0400

    Change DropConnect to Dropout with full noise_shape

--- NeuralNets_keras/utils.py
+++ NeuralNets_keras/utils.py
@@ -12,24 +12,15 @@ from keras.utils import tf_utils
 class DropConnect(keras.layers.Layer):
     def __init__(self, rate, **kwargs):
         super(DropConnect, self).__init__(**kwargs)
-        self.rate = rate # Need to match input data type
+        self.rate = rate
 
     def build(self, input_shape):
-        shape = input_shape[-1] 
-        # Sample Bernoulli Random Variable
-        boolean = tf.random.uniform(shape =(1,), minval = 0, maxval = 1) > self.rate
-        # Make mask_matrix
-        mask_matrix = tf.repeat(boolean, shape*shape)
-        mask_matrix = tf.reshape(mask_matrix, (shape, shape))
-        mask_matrix = tf.cast(mask_matrix, dtype = self.compute_dtype)
-        # Apply mask to the input
-        self.StochasticDrop = keras.layers.Lambda(lambda x: tf.matmul(x, mask_matrix))
-
+        num_shapes = len(input_shape)
+        shape = (None,)+(1,)*(num_shapes-1)
+        self.StochasticDrop = keras.layers.Dropout(self.rate, noise_shape = shape)
+        
     def call(self, inputs, training = None):
-        if training:
-            outputs = self.StochasticDrop(inputs)
-            return outputs
-        return inputs
+        return self.StochasticDrop(inputs)
     
     def compute_output_shape(self, input_shape):
         return input_shape[0]
@@ -38,3 +29,4 @@ class DropConnect(keras.layers.Layer):
         base_config = super().get_config()
         config = {"drop_rate": self.rate}
         return {**base_config, **config}
+    
